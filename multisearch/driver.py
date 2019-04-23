cdbf223212db939a6266f56d909f0c1a692cfd9f
Stephen Goeddel <sgoeddel247@gmail.com>
2019-04-23 19:33:14 -0400

    added a CLI

--- multisearch/driver.py
+++ multisearch/driver.py
@@ -1,10 +1,14 @@
 from multisearch.search import MultiSearch
 
 def run():
-    multisearch = MultiSearch('stephen goeddel', 10)
-    results = multisearch.search()
-    for result in results:
-        print(result)
+    while True:
+        print("\n")
+        query = input("What would you like to multi-search for?")
+        multisearch = MultiSearch(query, 10)
+        results = multisearch.search()
+        print("Results: ")
+        for result in results:
+            print(result)
 
 
 run()
